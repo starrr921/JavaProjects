775fcc341813fa235a15e9a35188ddb0bebb65b1
starrr921 <2446626295@qq.com>
2024-09-18 22:07:04 +0800

    20240918

---
+++ JavaProjects/ScannerDemo1.java
@@ -0,0 +1,16 @@
+
+
+import java.util.Scanner;
+
+
+public class ScannerDemo1 {
+    public static void main(String[] args){
+    
+        // 创建对象
+        Scanner sc = new Scanner(System.in);
+    
+        // 接收数据
+        int i = sc.nextInt();
+        System.out.println(i);
+    }
+}
---
+++ JavaProjects/puzzlegame/src/com/pzr/App.java
@@ -0,0 +1,10 @@
+package com.pzr.ui;
+
+public class App {
+    public static void main(String[] args) {
+
+        //表示程序的启动入口
+//        new LoginJframe();
+        new GameJframe();
+    }
+}
